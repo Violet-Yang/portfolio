716d6bd78e9793b687dd42a602cad5468734ce85
violet <violet91410@gmail.com>
2021-02-09 23:41:04 +0900

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: "Noto Sans KR";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 1366px;
+}
+
+p, div {
+  margin: 0;
+  padding: 0;
+}
+
+.flex-all-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+</style>
