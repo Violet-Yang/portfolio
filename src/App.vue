55cc42f5a3020006c52df2cd593e2af45d931ff6
violet <violet91410@gmail.com>
2021-02-19 02:45:29 +0900

    until_portfolio

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,26 @@ export default {
 @import "../assets/css/common.css";
 
 #app {
-  font-family: "Noto Sans KR";
+  font-family: "Noto Sans KR";;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-
+  /*color: #2c3e50;*/
+  color: rgba(35,35,35,1);
+  /*overflow-x: hidden;*/
 }
 
+body {
+  margin: 0;
+  padding: 0;
+}
 
 h1, p, div {
   margin: 0;
   padding: 0;
 }
 
-h2 {
+h2, h3 {
   font-weight: normal;
   margin: 0;
 }
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <div>
-      <section class="start-page" id="home">
-        <img src="static/img/giraffe.jpg" />
-      </section>
-      <section class="menu" style="background-color: white">
+    <div style="background-color: #f3f3f3; width: 100vw;">
+      <section
+        class="menu"
+        style="
+          background-color: #f3f3f3;
+          border-bottom: 1px solid lightgray;
+          max-width: 1000px;
+        "
+      >
         <div style="display: flex" class="menu-content">
           <div id="logo">Y.JIEUN</div>
           <div class="menu-list">
@@ -14,11 +18,21 @@
             <div class="click-menu"><a href="#home">BLOG</a></div>
           </div>
         </div>
+
+        <div
+          style="width: 100%; height: 100%; background-color: slateblue"
+          class="menu-content-s flex-align-center flex-between"
+        >
+          <div style="margin-left: 20px" id="logo-s">Y.JIEUN</div>
+          <div style="margin-right: 20px">
+            <img style="width: 100%" src="static/img/barbadak.png" />
+          </div>
+        </div>
       </section>
 
       <!--about me-->
       <section class="about-me" id="about-me">
-        <div class="content">
+        <div class="content1">
           <h1
             style="
               font-size: 34px;
@@ -29,71 +43,71 @@
             "
           >
             JUNIOR <br />
-            FRONT-END <br />
-            ENGINEER
+            FRONT-END ENGINEER
           </h1>
           <hr />
-          <p class="info">
+          <p style="margin-top: 40px" class="info">
             <span style="font-size: 25px; font-weight: bold">양지은</span>
             <br />
             1991.04.10<br />
             +82 10-6644-0155 <br />
             violet91410@gmail.com
           </p>
-        </div>
 
-        <!-- circles -->
-        <div class="circles">
-          <!--1st circle-->
-          <div class="circle-area">
-            <div class="motto" style="width: 100%">
-              <div style="display: flex; justify-content: center">
-                <div class="circle">
-                  <img style="width: 90%" src="static/img/dog.png" />
+          <!-- circles -->
+          <div class="circles">
+            <!--1st circle-->
+            <div class="circle-area">
+              <div class="motto">
+                <div style="display: flex; justify-content: center">
+                  <div class="circle">
+                    <img style="width: 90%" src="static/img/dog.png" />
+                  </div>
                 </div>
+                <h2>MOTTO</h2>
+                <p class="pen">
+                  <span style="color: #42b983; font-weight: bold"
+                    >도전하는 삶</span
+                  >을 지향하는 웹 개발자입니다. 자기계발로 시작했던 퍼블리싱에서
+                  신세계를 경험하고, 자바스크립트에 푹 빠져 프론트엔드 개발자가
+                  되었습니다.
+                </p>
               </div>
-              <h2>MOTTO</h2>
-              <p class="pen">
-                <span style="color: #42b983; font-weight: bold"
-                  >도전하는 삶</span
-                >을 지향하는 웹 개발자입니다. 자기계발로 시작했던 퍼블리싱에서
-                신세계를 경험하고, 자바스크립트에 푹 빠져 프론트엔드 개발자가
-                되었습니다.
-              </p>
             </div>
-          </div>
-          <!-- 2nd circle-->
-          <div class="circle-area">
-            <div class="motto" style="width: 100%">
-              <div style="display: flex; justify-content: center">
-                <div class="circle">
-                  <img style="width: 90%" src="static/img/dog.png" />
+            <!-- 2nd circle-->
+            <div class="circle-area">
+              <div class="motto">
+                <div style="display: flex; justify-content: center">
+                  <div class="circle">
+                    <img style="width: 90%" src="static/img/dog.png" />
+                  </div>
                 </div>
+                <h2>PUBLISHER</h2>
+                <p class="pen">
+                  <span style="font-weight: bold"
+                    >HTML5, CSS3, Javascript(ES6+), MEDIA QUERIES</span
+                  >를 사용하여 요구사항을 구현할 수 있습니다.
+                </p>
               </div>
-              <h2>PUBLISHER</h2>
-              <p class="pen">
-                <span style="font-weight: bold"
-                  >HTML5, CSS3, Javascript(ES6+), MEDIA QUERIES</span
-                >를 사용하여 요구사항을 구현할 수 있습니다.
-              </p>
             </div>
-          </div>
-          <!-- 3nd circle-->
-          <div class="circle-area">
-            <div class="motto" style="width: 100%">
-              <div style="display: flex; justify-content: center">
-                <div class="circle">
-                  <img style="width: 90%" src="static/img/dog.png" />
+            <!-- 3nd circle-->
+            <div class="circle-area">
+              <div class="motto">
+                <div style="display: flex; justify-content: center">
+                  <div class="circle">
+                    <img style="width: 90%" src="static/img/dog.png" />
+                  </div>
                 </div>
+                <h2>Front-end Developer</h2>
+                <p class="pen">
+                  웹의 전반적인 지식을 가지고 있으며,
+                  <span style="font-weight: bold">Vue</span>를 사용한
+                  <span style="font-weight: bold">Single Page Application</span>
+                  개발에 자신이 있습니다.<br />
+                  또한, 개발지식을 기억하고자 학습블로그를 기록하며
+                  공부중입니다.
+                </p>
               </div>
-              <h2>Front-end Developer</h2>
-              <p class="pen">
-                웹의 전반적인 지식을 가지고 있으며,
-                <span style="font-weight: bold">Vue</span>를 사용한
-                <span style="font-weight: bold">Single Page Application</span>
-                개발에 자신이 있습니다.<br />
-                또한, 개발지식을 기억하고자 학습블로그를 기록하고 있습니다.
-              </p>
             </div>
           </div>
         </div>
@@ -101,13 +115,13 @@
       <section
         class="portfolio"
         id="portfolio"
-        style="background-color: #f3f3f3"
+        style="background-color: #ffffff;"
       >
-        <div class="content">
+        <div class="content2">
           <h1
             style="
-              font-size: 34px;
-              letter-spacing: 6px;
+              font-size: 40px;
+              letter-spacing: 5px;
               line-height: 1.2;
               padding-top: 120px;
             "
@@ -115,57 +129,94 @@
             PORTFOLIO
           </h1>
           <hr />
-          <div class="portfolio-list" style="display: flex; margin-top: 120px">
+          <div
+            class="portfolio-list"
+            style="display: flex; justify-content: center; margin-top: 50px;"
+          >
             <div
               class="each"
               style="
-                width: 1200px;
-                height: 100%;
-                background-color: white;
+                margin: 0 auto;
                 padding: 10px;
                 text-align: left;
               "
             >
-              <div style="display: flex">
-                <div class="text-area">
-                  <h2>01</h2>
-                  <p>별별귤팜</p>
-                  <p>서비스 출시</p>
+              <div class="area" style="border-left: 3px solid black;">
+                <div class="text-area" style="padding-left : 10px">
+                  <h2 style="font-size: 15px">2020.09 ~ 2021.01</h2>
+                  <h2 style="letter-spacing: -1.4px">별별귤팜</h2>
+
                   <div class="overview">
-                    <div class="flex-align-center">
+                    <div class="flex-align-center" style="margin-top: 5px">
                       <img
-                        style="width: 3%"
-                        src="static/img/ico_menu_.png"
-                      /><span class="description">Overview</span>
+                        style="width: 6%; max-width: 30px"
+                        src="static/img/icon_github.png"
+                      /><span class="description"
+                        ><a href="">Github Link</a></span
+                      >
                     </div>
-                    <p style="margin: 15px 0; width: 500px">
-                      제주도 산 귤, 제주도 현지농장을 소개하고 판매하는
-                      플랫폼입니다.
-                    </p>
-                  </div>
-                  <div class="stack">
-                    <div class="flex-align-center">
-                      <img style="width: 3%" src="static/img/ico_stack_.png" />
-                      <span class="description">Stack</span>
-                    </div>
-                    <div style="margin-top: 10px">
-                      <span class="radius">Vue</span>
-                      <span class="radius">Rest API</span>
-                    </div>
-                  </div>
-                  <div class="link">
-                    <div class="flex-align-center">
-                      <img style="width: 3%" src="static/img/ico_link_.png" />
-                      <span class="description">Link</span>
-                    </div>
-                    <div style="margin-top: 10px">
-                      <span class="radius">Service</span>
-                      <span class="radius">Github</span>
+                    <div class="flex-align-center" style="margin-top: 7px">
+                      <img
+                        style="width: 6%; max-width: 30px"
+                        src="static/img/icon_globe.png"
+                      /><span class="description"
+                        ><a href="https://www.bbgfarm.shop/">Site Link</a></span
+                      >
                     </div>
                   </div>
                 </div>
-                <div class="img-area">
-                  <img style="width: 90%" src="static/img/bbgf.png" />
+                  <div class="img-area" style="padding-left: 10px">
+                    <img src="static/img/img_bbgfam.jpg" />
+                  </div>
+
+                  <div class="img-area-s">
+                    <div>
+                      <img
+                        style="width: 100%"
+                        src="static/img/img_bbgfam_s.JPG"
+                      />
+                    </div>
+                  </div>
+
+                  <p
+                    class="intro"
+                    style="
+                      width: 100%;
+                      font-size: 14px;
+                      font-weight: 400;
+                      line-height: 1.6;
+                      margin: 5px 0;
+                      letter-spacing: -0.9px;
+                      padding-left : 10px
+                    "
+                  >
+                    제주도 산 귤, 농장을 소개하고 판매하는 플랫폼
+                    <br />
+                    5인 팀에서 프론트엔드 일부 개발 담당
+                  </p>
+
+                <div class="between-box" style="max-width: 1200px; margin: 15px">
+                  <div class="between-box-in1">
+                    <h3 style="font-weight: 550; letter-spacing: -0.8px">
+                      What I did
+                    </h3>
+                    <div class="underline"></div>
+                    <p class="contain-text">
+                      메인, 농장, 장바구니 화면 및 기능 개발
+                    </p>
+                  </div>
+
+                  <div class="between-box-in2">
+                    <h3 style="font-weight: 550; letter-spacing: -0.8px">
+                      Stack
+                    </h3>
+                    <div class="underline"></div>
+                    <p class="contain-text">
+                      HTML5/CSS3 기반 퍼플리싱<br />
+                      Vue.js 활용 프론트엔드 기능 구현<br />
+                      Rest API 연동
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
@@ -174,9 +225,26 @@
       </section>
 
       <!-- contact -->
-      <section class="portfolio" id="contact" style="background-color: #f3f3f3">
-        <div class="content">
-          <h1 style="font-size: 34px; letter-spacing: 6px; line-height: 1.2">
+      <section class="contact" id="contact" style="background-color: #f3f3f3">
+        <div class="content3">
+          <h1
+            style="
+              font-size: 40px;
+              letter-spacing: 5px;
+              padding-top: 120px;
+              line-height: 1.2;
+            "
+          >
+            BLOG
+          </h1>
+          <hr />
+          <h1 style="font-size: 40px; letter-spacing: 5px; line-height: 1.2">
+            CONTACT
+          </h1>
+          <h1 style="font-size: 40px; letter-spacing: 5px; line-height: 1.2">
+            CONTACT
+          </h1>
+          <h1 style="font-size: 40px; letter-spacing: 5px; line-height: 1.2">
             CONTACT
           </h1>
         </div>
@@ -243,6 +311,15 @@ hr {
   text-align: left;
   font-weight: bold;
 }
+#logo-s {
+  line-height: 60px; /* <-- 해당 css로 옆의 menu flex속성이 적용됨 */
+  font-size: 24px;
+  text-align: left;
+  font-weight: bold;
+  color: white;
+}
+
+/* 전체 탑 네비게이션 바*/
 .menu-list {
   display: flex;
 }
@@ -256,15 +333,16 @@ hr {
   justify-content: center;
 }
 /* about me 부터 content영역 */
-.about-me, .content {
-  width: 900px;
-  margin: 0 auto;
-}
+/*#about-me, .content {*/
+/*  width: 100%;*/
+/*  max-width: 1200px;*/
+/*  margin: 0 auto;*/
+/*}*/
 
 /* 내정보 */
 .info {
-  color: #a4a4a4;
-  margin: 40px auto;
+  color: #757575;
+  margin: 0 auto;
   width: 300px;
   line-height: 25px;
   letter-spacing: 1px;
@@ -289,8 +367,7 @@ hr {
 }
 /* about me 하단 작은제목 영역 */
 .motto {
-  font-weight: lighter;
-  width: 300px;
+  width: 100%;
   height: 250px;
   margin-top: 40px;
   margin-bottom: 120px;
@@ -305,20 +382,53 @@ hr {
   font-size: 14px;
   width: 260px;
   margin: 30px auto;
-  color: #a4a4a4;
+  /*color: #a4a4a4;*/
+  /*color: #808080;*/
+  color: #757575;
   line-height: 24px;
   letter-spacing: 0;
   font-weight: 300;
 }
+
+/* 포트폴리오 영역 큰 div */
+/*.area {*/
+/*  display: flex;*/
+/*}*/
+
+/* 아이콘 설명 글씨 */
 .description {
-  margin: 10px 0;
-  font-size: 20px;
-  font-weight: bold;
-  letter-spacing: -0.2px;
+  color: rgba(63, 63, 63, 1);
   margin-left: 10px;
-  color: #2c3e50;
 }
 
+.description a {
+  font-size: 21px;
+  letter-spacing: -0.6px;
+  color: goldenrod;
+}
+
+.description a:hover {
+  text-decoration: underline;
+}
+/* 소제목 밑줄 */
+.underline {
+  width: 200px;
+  height: 1px;
+  margin: 3px 0;
+  background-color: rgb(52, 58, 64);
+}
+/* 설명영역 */
+.contain-text {
+  /*padding: 15px 10px;*/
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: -0.5px;
+  font-weight: 300;
+
+  color: #232323;
+}
+
+/* 영역 안 키워드 */
 .radius {
   padding: 5px 10px;
   border-radius: 10px;
@@ -327,42 +437,119 @@ hr {
 }
 
 /* 미디어쿼리 -> 반응형 */
-@media (min-width: 768px) and (max-width: 2000px) {
+@media (min-width: 320px) and (max-width: 767px) {
+  .menu {
+    background-color: blueviolet;
+    width: 100%;
+    height: 60px;
+  }
+
+  #logo,
+  .menu-list {
+    display: none !important;
+  }
+  .menu-content {
+    display: none;
+  }
+
+  .circles {
+    display: block;
+  }
+
+  .circle-area {
+    width: 100%;
+  }
+
+  .img-area-s {
+    margin-top: 20px;
+    margin-left: 10px;
+
+  }
+  .img-area {
+    display: none;
+  }
+  .between-box {
+    display: block;
+  }
+  .between-box-in2 {
+    margin-top: 10px;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
   #logo {
     /*display: flex;*/
+    width: 200px;
+    margin: 0 auto;
+  }
+
+  .menu-content-s {
+    display: none;
+  }
+
+  /*.content1 {*/
+  /*  margin: 0 auto;*/
+  /*}*/
+
+  .circles {
+    display: flex;
+    justify-content: center;
+  }
+
+  .img-area-s {
+    display: none;
+  }
+  .img-area {
+    margin-left: 20px;
+  }
+  .between-box {
+    display: flex;
+  }
+  .between-box-in1,
+  .between-box-in2 {
+    width: 50%;
+  }
+}
+
+@media (min-width: 1024px) and (max-width: 3000px) {
+  .menu {
+    margin: 0 auto;
+  }
+
+  #logo {
+    display: flex;
     width: 300px;
     margin: 0 auto;
   }
   .menu-list {
     width: 500px;
   }
-  .circles {
-    display: flex;
+  .menu-content-s {
+    display: none;
   }
-}
 
-@media (min-width: 580px) and (max-width: 768px) {
-  #logo {
-    /*display: flex;*/
-    width: 200px;
-    margin: 0 auto;
-  }
   .circles {
     display: flex;
+    justify-content: center;
   }
-}
-@media (min-width: 320px) and (max-width: 579px) {
-  .menu {
-    background-color: blueviolet;
-    width: 100%;
-    height: 60px;
+
+  .circles,
+  area {
+    display: flex;
   }
-  #logo,
-  .menu-list {
-    display: none !important;
+
+  .img-area-s {
+    display: none;
   }
-  .circle-area {
-    width: 300px;
+  .img-area {
+    margin-left: 20px;
+  }
+  .between-box {
+    display: flex;
+  }
+  .between-box-in1,
+  .between-box-in2 {
+    width: 50%;
   }
 }
 </style>
